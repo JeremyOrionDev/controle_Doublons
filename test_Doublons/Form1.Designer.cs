8f0c4e67a5a655578140b8bc611d52254fd53315
jeremyorionDev <jeremy9315@hotmail.fr>
2017-11-20 15:03:47 +0100

    commity

--- test_Doublons/Form1.Designer.cs
+++ test_Doublons/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnFichier = new System.Windows.Forms.Button();
+            this.tBxAdress = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblColonnes = new System.Windows.Forms.Label();
+            this.tBxColonnes = new System.Windows.Forms.TextBox();
+            this.lblLignes = new System.Windows.Forms.Label();
+            this.tBxLigne = new System.Windows.Forms.TextBox();
+            this.lblFeuille = new System.Windows.Forms.Label();
+            this.tBxFeuilles = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFichier
+            // 
+            this.btnFichier.Location = new System.Drawing.Point(21, 25);
+            this.btnFichier.Name = "btnFichier";
+            this.btnFichier.Size = new System.Drawing.Size(75, 23);
+            this.btnFichier.TabIndex = 0;
+            this.btnFichier.Text = "Fichier";
+            this.btnFichier.UseVisualStyleBackColor = true;
+            this.btnFichier.Click += new System.EventHandler(this.btnFichier_Click);
+            // 
+            // tBxAdress
+            // 
+            this.tBxAdress.Location = new System.Drawing.Point(134, 27);
+            this.tBxAdress.Name = "tBxAdress";
+            this.tBxAdress.ReadOnly = true;
+            this.tBxAdress.Size = new System.Drawing.Size(241, 20);
+            this.tBxAdress.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblColonnes
+            // 
+            this.lblColonnes.AutoSize = true;
+            this.lblColonnes.Location = new System.Drawing.Point(18, 115);
+            this.lblColonnes.Name = "lblColonnes";
+            this.lblColonnes.Size = new System.Drawing.Size(51, 13);
+            this.lblColonnes.TabIndex = 2;
+            this.lblColonnes.Text = "Colonnes";
+            // 
+            // tBxColonnes
+            // 
+            this.tBxColonnes.Location = new System.Drawing.Point(134, 112);
+            this.tBxColonnes.Name = "tBxColonnes";
+            this.tBxColonnes.ReadOnly = true;
+            this.tBxColonnes.Size = new System.Drawing.Size(241, 20);
+            this.tBxColonnes.TabIndex = 3;
+            // 
+            // lblLignes
+            // 
+            this.lblLignes.AutoSize = true;
+            this.lblLignes.Location = new System.Drawing.Point(18, 156);
+            this.lblLignes.Name = "lblLignes";
+            this.lblLignes.Size = new System.Drawing.Size(38, 13);
+            this.lblLignes.TabIndex = 2;
+            this.lblLignes.Text = "Lignes";
+            // 
+            // tBxLigne
+            // 
+            this.tBxLigne.Location = new System.Drawing.Point(134, 153);
+            this.tBxLigne.Name = "tBxLigne";
+            this.tBxLigne.ReadOnly = true;
+            this.tBxLigne.Size = new System.Drawing.Size(241, 20);
+            this.tBxLigne.TabIndex = 3;
+            // 
+            // lblFeuille
+            // 
+            this.lblFeuille.AutoSize = true;
+            this.lblFeuille.Location = new System.Drawing.Point(18, 71);
+            this.lblFeuille.Name = "lblFeuille";
+            this.lblFeuille.Size = new System.Drawing.Size(42, 13);
+            this.lblFeuille.TabIndex = 2;
+            this.lblFeuille.Text = "Feuilles";
+            // 
+            // tBxFeuilles
+            // 
+            this.tBxFeuilles.Location = new System.Drawing.Point(134, 68);
+            this.tBxFeuilles.Name = "tBxFeuilles";
+            this.tBxFeuilles.ReadOnly = true;
+            this.tBxFeuilles.Size = new System.Drawing.Size(241, 20);
+            this.tBxFeuilles.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 262);
+            this.Controls.Add(this.tBxLigne);
+            this.Controls.Add(this.tBxFeuilles);
+            this.Controls.Add(this.tBxColonnes);
+            this.Controls.Add(this.lblFeuille);
+            this.Controls.Add(this.lblLignes);
+            this.Controls.Add(this.lblColonnes);
+            this.Controls.Add(this.tBxAdress);
+            this.Controls.Add(this.btnFichier);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFichier;
+        private System.Windows.Forms.TextBox tBxAdress;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblColonnes;
+        private System.Windows.Forms.TextBox tBxColonnes;
+        private System.Windows.Forms.Label lblLignes;
+        private System.Windows.Forms.TextBox tBxLigne;
+        private System.Windows.Forms.Label lblFeuille;
+        private System.Windows.Forms.TextBox tBxFeuilles;
     }
 }
 
--- test_Doublons/Form1.cs
+++ test_Doublons/Form1.cs
@@ -1,20 +1,155 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel=Microsoft.Office.Interop.Excel;
+
 
 namespace test_Doublons
 {
     public partial class Form1 : Form
     {
+
+        Boolean estOuvert;
+
+        private string fileAdress;
+        int j ;
+        string[] decoupe;
+        Excel.Application xlsApp = new Excel.Application();
+        StringComparer SC = StringComparer.Ordinal;
+        StringComparer Test = StringComparer.Ordinal;
+        
+        Dictionary<string,int> colonne1=new Dictionary<string, int>();
+        Dictionary<string, int> doublons1 = new Dictionary<string, int>();
+        Dictionary<string, int> colonne2 = new Dictionary<string, int>();
+        Dictionary<string, int> doublons2 = new Dictionary<string, int>();
+        Dictionary<string, int> colonne3 = new Dictionary<string, int>();
+        Dictionary<string, int> doublons3 = new Dictionary<string, int>();
+        Dictionary<string, int> colonne4 = new Dictionary<string, int>();
+        Dictionary<string, int> doublons4 = new Dictionary<string, int>();
+        Workbook xlsWorkbook;
         public Form1()
         {
+    
+            //MessageBox.Show("le resultat de la comparaison:: " + Test.Compare("Bonjour mlle, comment allez vous?", "Bonjour mlle, comment allez vous?"));
             InitializeComponent();
         }
+        /// <summary>
+        /// Check wether a file is locked
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+        private void btnFichier_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            openFileDialog1 = new OpenFileDialog();
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileAdress = tBxAdress.Text = openFileDialog1.FileName;
+                FileInfo F = new FileInfo(fileAdress);
+                if (IsFileLocked(F))
+                {
+                    MessageBox.Show("fichier deja ouvert");
+                    xlsApp.Workbooks.Close();
+                }
+
+                xlsWorkbook = xlsApp.Workbooks.Open(fileAdress, Type.Missing);
+
+
+
+                _Worksheet xlsWorksheet = xlsWorkbook.Sheets[xlsWorkbook.Sheets.Count];
+                Range range = xlsWorksheet.UsedRange;
+                tBxFeuilles.Text = xlsWorksheet.Name;
+                tBxColonnes.Text = range.Columns.Count.ToString();
+                tBxLigne.Text = range.Rows.Count.ToString();
+                Excel.Worksheet export;
+                export = (Excel.Worksheet)xlsWorkbook.Worksheets.Add();
+                MessageBox.Show("test colonne 1 ligne 25 ..." + range.Columns[1].cells[25].value);
+                for (int i = 1; i <= range.Columns.Count; i++)
+                {
+                    
+                    for (int j = 1; j <= range.Columns[i].cells.Count; j++)
+                    {
+                        for (int k = 1; k <= range.Columns[i].cells.Count; k++)
+                        {
+                            if (j != range.Columns[i].cells.Count && j != k)
+                            {
+
+                                if (range.Columns[i].cells[j].value is string && range.Columns[i].cells[k].value is string)
+                                {
+                                    if (SC.Compare(range.Columns[i].cells[j].value, range.Columns[i].cells[k].value) == 0)
+                                    {
+                                        MessageBox.Show("les cellules " + j + " et " + k + " sont identiques " + "\n" + range.Columns[i].cells[j].value + "\n" + range.Columns[i].cells[k].value);
+                                        range.Columns.Insert(i);
+                                        range.Columns[i + 1].cells[j].Value2 = "doublons ligne =" + k;
+                                        range.Columns[i + 1].cells[k].value = "doublon ligne =" + j;
+                                        
+                                    }
+                                }
+
+                                if (range.Columns[i].cells[j].value is double && range.Columns[i].cells[k].value is double )
+                                {
+                                    double D1 = range.Columns[i].cells[j].value;
+                                    double D2 = range.Columns[i].cells[k].value;
+                                    if (D1==D2)
+                                    {
+                                        MessageBox.Show("les cellules " + j + " et " + k + " sont identiques " + "\n" + range.Columns[i].cells[j].value + "\n" + range.Columns[i].cells[k].value);
+                                    }
+                                }
+
+
+
+                            }
+                            
+                        }
+                    }
+                    
+                    {
+
+                    }
+                }
+
+                xlsWorkbook.Save();
+                xlsWorkbook.Close(fileAdress);
+                MessageBox.Show("fin des tests");
+
+            }
+
+        }
+
     }
 }
